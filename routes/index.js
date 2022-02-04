d05b1cfef3c3f2d1bbcef23d17c220aad65e0277
Eduardo Beyra <eduardobeyra@hotmail.com>
2022-02-04 09:03:44 -0500

    iterations 1 through 4

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((results) => {
+      res.render("movies", { allMovies: results });
+    })
+    .catch((err) => {
+      console.log("Something went wrong", err);
+    });
+});
+
+router.get("/details/:id", (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then((results) => {
+      res.render("details", results);
+    })
+    .catch((err) => {
+      console.log("Something went wrong", err);
+    });
+});
 
 module.exports = router;
